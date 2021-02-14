28ed1065fb3c7cf807c3fd5f11f46e57691d945a
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-13 18:02:19 -0600

    Setup action types

---
+++ Client/store/actions/action_types.js
@@ -0,0 +1,15 @@
+// Boiler plate actions
+export const SUCCESS = "SUCCESS";
+export const FAILURE = "FAILURE";
+
+// Update authentication state
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+
+// Save the profile data from Auth0 to the global state
+export const ADD_PROFILE = "ADD_PROFILE";
+export const REMOVE_PROFILE = "REMOVE_PROFILE";
+
+// Track the changes and submit of the user submitted text of the form
+export const USER_INPUT_CHANGE = "USER_INPUT_CHANGE";
+export const USER_INPUT_SUBMIT = "USER_INPUT_SUBMIT";
