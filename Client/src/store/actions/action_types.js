09b018cbdf20af63139195e1d9d4cb119916266f
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-14 15:04:02 -0600

    Update actions with profile and posts actions

--- Client/src/store/actions/action_types.js
+++ Client/src/store/actions/action_types.js
@@ -13,3 +13,11 @@ export const REMOVE_PROFILE = "REMOVE_PROFILE";
 // Track the changes and submit of the user submitted text of the form
 export const USER_INPUT_CHANGE = "USER_INPUT_CHANGE";
 export const USER_INPUT_SUBMIT = "USER_INPUT_SUBMIT";
+
+// Save the profile data from Auth0 to the DB
+export const SET_DB_PROFILE = "SET_DB_PROFILE";
+export const REMOVE_DB_PROFILE = "REMOVE_DB_PROFILE";
+
+// Fetch post data to the DB
+export const FETCH_DB_POSTS = "FETCH_DB_POSTS";
+export const REMOVE_DB_POSTS = "REMOVE_DB_POSTS";
