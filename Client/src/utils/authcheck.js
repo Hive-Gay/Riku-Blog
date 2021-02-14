c27c7333addcf988e10f773586db60493abfe1e2
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-14 14:57:37 -0600

    Add authcheck for getting the user profile from db

--- Client/src/utils/authcheck.js
+++ Client/src/utils/authcheck.js
@@ -9,15 +9,20 @@ const AuthCheck = () => {
 
     useEffect(() => {
         if (context.authObj.isAuthenticated()) {
+            const profile = context.authObj.userProfile;
             context.handleUserLogin();
-            context.handleUserAddProfile(context.authObj.userProfile);
-            history.replace('/');
+            context.handleUserAddProfile(profile);
+            axios.post('/api/posts/userprofiletodb', profile)
+                .then(axios.get('/api/get/userprofilefromdb', {params: {email: profile.profile.email}})
+                .then(res => context.handleAddDBProfile(res.data)))
+                .then(history.replace('/'));
         } else {
             context.handleUserLogout();
             context.handleUserRemoveProfile();
+            context.handleUserRemoveProfile();
             history.replace('/');
         }
-    }, []);
+    }, [context.authObj.userProfile, context]);
 
     return (
         <div>
